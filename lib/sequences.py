a8cd20a779553e1cc7b3e417c4171aed6db639b9
kaan <kabuke13@gmail.com>
2024-01-20 20:07:31 +0000

    lab changes

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list = [0, 1]
+
+    if(length == 0):
+        my_list = []
+
+    elif(length == 1):
+        my_list = [0]
+
+    elif(length > 2):
+        while len(my_list) < length:
+            my_list.append(my_list[-1] + my_list[-2])
+    
+    print(my_list)
